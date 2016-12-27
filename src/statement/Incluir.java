bfa5d232f0b441b721f4658de45d44e33e767b8b
Renan <Renan@192.168.0.6>
2016-12-27 19:30:56 -0300

    Statement Incluir...

--- src/statement/Incluir.java
+++ src/statement/Incluir.java
@@ -33,28 +33,39 @@ public class Incluir extends JFrame implements ActionListener{
 		
 	}
 	
-	public void actionPerformed(Action e)
+	public void actionPerformed(ActionEvent e)
 	{
 		try
 		{
 			ComercioConexao BDComerce = new ComercioConexao();
-			PreparedStatement pst = BDComerce.getConexao().prepareStatement(
+			PreparedStatement statement = BDComerce.getConexao().prepareStatement(
 					"INSERT INTO Categoria (Descricao) Values(?)",Statement.RETURN_GENERATED_KEYS);
-			pst.setString(1, this.tfDetail.getText());
-			pst.executeUpdate();
+			statement.setString(1, this.tfDetail.getText());
+			statement.executeUpdate();
 			
 			BDComerce.confirmarTransacao();
 			
-			ResultSet rs = pst.getGeneratedKeys();
+			ResultSet rs = statement.getGeneratedKeys();
 			rs.next();
 			
 			showMessageDialog(this,"Registro Gravado: " + rs.getInt(1)
 				+ "-" + this.tfDetail.getText());
 			
 			rs.close();
-			
+			statement.close();
+						
+			this.tfDetail.setText("");
+			this.tfDetail.requestFocus();
 			
 		}
+		
+		catch(Exception ex)
+		{
+			showMessageDialog(this,ex.getMessage(),"Error",ERROR_MESSAGE);
+		}
+		
 	}
+	
+	
 	
 }
