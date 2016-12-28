137f30d6c4ad0e82656548f39f2f240a1f6245ab
Renan Thiago <renannojosa@gmail.com>
2016-12-28 20:12:52 -0300

    Update Exclui.java

--- src/statement/Exclui.java
+++ src/statement/Exclui.java
@@ -1,5 +1,37 @@
 package statement;
+import static javax.swing.JOptionPane.*;
+import conexao.ComercioConexao;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import java.sql.PreparedStatement;
 
-public class Exclui {
-
+public class Exclui extends JFrame implements ActionListener {
+	private JPanel pnSul;
+	private JFormattedTextField tfCodigo;
+	private JButton btDelete;
+	
+	public Exclui()
+	{
+		setTitle("Excluir Categoria");
+		setSize(200,100);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		this.pnSul = new JPanel();
+		this.tfCodigo = new JFormattedTextField(new Integer(0));
+		this.btDelete = new JButton("Delete");
+		this.btDelete.setMnemonic('D');
+		this.pnSul.add(this.btDelete);
+		
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(this.tfCodigo, BorderLayout.CENTER);
+		getContentPane().add(this.pnSul, BorderLayout.SOUTH);
+		
+	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		//Implemento depois
+	}
+	
 }
